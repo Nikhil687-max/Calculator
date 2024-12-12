9b345ca0e57c57a64c4dbe1086912c36e696a359
Nikhil Patidar <patidarnikhil687@gmail.com>
2024-12-12 17:33:44 +0530

    nearly complision of the project just will add some of many fancy stuff to add more to my calculator.

--- src/Display.jsx
+++ src/Display.jsx
@@ -6,7 +6,36 @@ let pans = 0;
 
 function ifError(s){return 1;}
 function isCalculate(s){return 1;}
-function calculate(s){return s[0];}
+var calculate = function (s) {
+    let number = 0;
+    let signValue = 1;
+    let result = 0;
+    let operationsStack = [];
+
+    for (let i = 0; i < s.length; i++) {
+        let c = s[i];
+
+        if (!isNaN(parseInt(c))) {
+            number = number * 10 + parseInt(c);
+        } else if (c === '+' || c === '-') {
+            result += number * signValue;
+            signValue = (c === '-') ? -1 : 1;
+            number = 0;
+        } else if (c === '(') {
+            operationsStack.push(result);
+            operationsStack.push(signValue);
+            result = 0;
+            signValue = 1;
+        } else if (c === ')') {
+            result += signValue * number;
+            result *= operationsStack.pop();
+            result += operationsStack.pop();
+            number = 0;
+        }
+    }
+
+    return result + number * signValue;
+}
 
 function append(x){
     console.log(x);
@@ -20,28 +49,36 @@ function append(x){
     }
     else if(x === 'M'){
         if(ans < 0){
-            document.getElementById('prev').textContent = ans;
-            pans = ans;
             ans *= -1;
-            document.getElementById('exp').textContent = 'ans*(-1)';
             document.getElementById('ans').textContent = ans;
-            s = "ans";
         }
     }
     else if(x === '='){
-        document.getElementById('prev').textContent = s;
-        document.getElementById('exp').textContent = "";
+        document.getElementById('prev').textContent = pans;
+        document.getElementById('exp').textContent = ans;
+        document.getElementById('ans').textContent = "";
+    }
+    else if(x === 'b'){
+        let s2 = "";
+        let i = 1;
+        if(s.length > 1){
+            while(s[i] != '\0'){
+                s2+= s[i-1];
+                i++;
+            }
+        }
+        s = s2;
+        document.getElementById('prev').textContent = pans;
+        document.getElementById('exp').textContent = s;
+        ans = calculate(s);
         document.getElementById('ans').textContent = ans;
-        s = 'ans '
     }
     else 
     {
         s+= x;
         document.getElementById('exp').textContent = s;
         if(isCalculate(s)){
-            pans = ans;
             ans = calculate(s);
-            document.getElementById('prev').textContent = pans;
             document.getElementById('ans').textContent = ans;
         }
         else if(ifError(s)){
@@ -62,6 +99,35 @@ function Display(){
                 <h1 id="exp" className="main"></h1>
                 <h1 id="ans" className="main"></h1>
             </div>
+            <div id="row0" style={{display:"flex"}}>
+                <div id="btn" onClick={() => append('(')}>M+</div>
+                <div id="btn" onClick={() => append('0')}>M-</div>
+                <div id="btn" onClick={() => append(')')}>MR</div>
+                <div id="btn" onClick={() => append('=')}>MC</div>
+            </div>
+
+
+
+            <div id="row01" style={{display:"flex", marginLeft: 150}}>
+                <div id="btn" onClick={() => append('^')}>/\</div>
+                <div id="btn" onClick={() => append('b')}>del</div>
+
+            </div>
+            <div id="row02" style={{display:"flex", marginLeft: 48}}>
+                <div id="btn" onClick={() => append('<-')}>/</div>
+                <div id="btn" onClick={() => append('++')}>++</div>
+                <div id="btn" onClick={() => append('<-')}>\</div>
+            </div> 
+            <div id="row02" style={{display:"flex", marginLeft: 48}}>
+                <div id="btn" onClick={() => append('<-')}>\</div>
+                <div id="btn" onClick={() => append('--')}>--</div>
+                <div id="btn" onClick={() => append('->')}>/</div>
+            </div>
+            <div id="row03" style={{display:"flex", marginLeft: 150}}>
+                <div id="btn" onClick={() => append('6')}>\/</div>
+            </div>
+
+
             <div id="row1" style={{display:"flex"}}>
                 <div id="btn" onClick={() => append('C')}>C</div>
                 <div id="btn" onClick={() => append('M')}>Mod</div>
@@ -87,9 +153,9 @@ function Display(){
                 <div id="btn" onClick={() => append('/')}>/</div>
             </div>
             <div id="row5" style={{display:"flex"}}>
-                <div id="btn"> </div>
+                <div id="btn" onClick={() => append('(')}>(</div>
                 <div id="btn" onClick={() => append('0')}>0</div>
-                <div id="btn"> </div>
+                <div id="btn" onClick={() => append(')')}>)</div>
                 <div id="btn" onClick={() => append('=')}>=</div>
             </div>
         </>
